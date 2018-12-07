ea506aad1d9779d38b507b9e341f2f70d8ee76b3
AlejandroBudy <atorresato@gmail.com>
2018-12-07 19:05:33 +0100

    formatTemplateCorrectly test and consecutive number implemented

--- src/test/java/es/upm/grise/profundizacion2018/tema5/SmokeTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/tema5/SmokeTest.java
@@ -1,21 +1,37 @@
 package es.upm.grise.profundizacion2018.tema5;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
+import java.util.Properties;
+
 public class SmokeTest {
-	
+
+	class DocumentIdProviderDouble extends DocumentIdProvider{
+		DocumentIdProviderDouble(Properties customProperties) throws NonRecoverableError {
+			initClass(customProperties);
+		}
+	}
+
 	@Test
 	public void formatTemplateCorrectly() throws NonRecoverableError, RecoverableError {
-		
 		Document d = new Document();
+		int documentId = d.getDocumentId();
 		d.setTemplate("DECLARATION");
 		d.setTitle("A");
 		d.setAuthor("B");
 		d.setBody("C");
-		assertEquals("DOCUMENT ID: 1115\n\nTitle : A\nAuthor: B\n\nC", d.getFormattedDocument());
-
+		assertEquals("DOCUMENT ID: "+documentId+"\n\nTitle : A\nAuthor: B\n\nC", d.getFormattedDocument());
 	}
+
+	@Test
+	public void shouldReturnConsecutiveNumbers() throws NonRecoverableError {
+		Document d = new Document();
+		int documentId1 = d.getDocumentId();
+		Document d2 = new Document();
+		int documentId2 = d.getDocumentId();
+		assertEquals(documentId2,documentId1+1);
+	}
+
 
 }
