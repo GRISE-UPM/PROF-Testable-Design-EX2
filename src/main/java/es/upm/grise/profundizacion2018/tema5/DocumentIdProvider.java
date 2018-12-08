9d7de446a7cc0971146747824d6752d7e9117868
Miguel <magasuan@gmail.com>
2018-12-08 15:52:45 +0100

    Finished tema5 ejercicio

--- src/main/java/es/upm/grise/profundizacion2018/tema5/DocumentIdProvider.java
+++ src/main/java/es/upm/grise/profundizacion2018/tema5/DocumentIdProvider.java
@@ -17,6 +17,7 @@ import java.util.Properties;
 public class DocumentIdProvider {
 
 	// Environment variable
+	static String DRIVER = "com.mysql.jdbc.Driver";
 	private static String ENVIRON  = "APP_HOME";
 
 	// ID for the newly created documents
@@ -28,21 +29,21 @@ public class DocumentIdProvider {
 	// Singleton access
 	private static DocumentIdProvider instance;
 
-	public static DocumentIdProvider getInstance() throws NonRecoverableError {
+	public static DocumentIdProvider getInstance(String driver) throws NonRecoverableError {
 		if (instance != null)
 
 			return instance;
 
 		else {
 
-			instance = new DocumentIdProvider();
+			instance = new DocumentIdProvider(driver);
 			return instance;
 
 		}	
 	}
 
 	// Create the connection to the database
-	private DocumentIdProvider() throws NonRecoverableError {
+	private DocumentIdProvider(String driver) throws NonRecoverableError {
 
 		// If ENVIRON does not exist, null is returned
 		String path = System.getenv(ENVIRON);
@@ -54,65 +55,9 @@ public class DocumentIdProvider {
 
 		} else {
 
-			Properties propertiesInFile = new Properties();
-			InputStream inputFile = null;
-
-			// Load the property file
-			try {
-				inputFile = new FileInputStream(path + "config.properties");
-				propertiesInFile.load(inputFile);
-
-			} catch (FileNotFoundException e) {
-
-				System.out.println(NON_EXISTING_FILE.getMessage());          	
-				throw new NonRecoverableError();
-
-			} catch (IOException e) {
-
-				System.out.println(CANNOT_READ_FILE.getMessage());          	
-				throw new NonRecoverableError();
-
-			}
-
-			// Get the DB username and password
-			String url = propertiesInFile.getProperty("url");
-			String username = propertiesInFile.getProperty("username");
-			String password = propertiesInFile.getProperty("password");
-
-			// Load DB driver
-			try {
-
-				Class.forName("com.mysql.jdbc.Driver").newInstance();
-
-			} catch (InstantiationException e) {
-
-				System.out.println(CANNOT_INSTANTIATE_DRIVER.getMessage());          	
-				throw new NonRecoverableError();
-
-			} catch (IllegalAccessException e) {
-
-				System.out.println(CANNOT_INSTANTIATE_DRIVER.getMessage());          	
-				throw new NonRecoverableError();
-
-			} catch (ClassNotFoundException e) {
-
-				System.out.println(CANNOT_FIND_DRIVER.getMessage());          	
-				throw new NonRecoverableError();
-
-			}
-
-			// Create DB connection
-			try {
-
-				connection = DriverManager.getConnection(url, username, password);
-
-			} catch (SQLException e) {
-
-				System.out.println(CANNOT_CONNECT_DATABASE.getMessage());          	
-				throw new NonRecoverableError();
-
-			}
-
+			Properties propertiesInFile = readProp(path);
+			connection = readFromDB(driver, propertiesInFile);
+			//InputStream inputFile = null;
 			// Read from the COUNTERS table
 			String query = "SELECT documentId FROM Counters";
 			Statement statement = null;
@@ -171,6 +116,64 @@ public class DocumentIdProvider {
 		}
 	}
 
+	Properties readProp(String path) throws NonRecoverableError {
+		Properties propertiesInFile  = new Properties();
+		InputStream inputFile = null;
+		try {
+
+			inputFile = new FileInputStream(path + "config.properties");
+			propertiesInFile .load(inputFile);
+
+		} catch (FileNotFoundException e) {
+
+			throw new NonRecoverableError(NON_EXISTING_FILE.getMessage());
+
+		} catch (IOException e) {
+
+			throw new NonRecoverableError(CANNOT_READ_FILE.getMessage());
+		}
+
+		return propertiesInFile ;
+	}
+
+	Connection readFromDB(String driver, Properties prop) throws NonRecoverableError{
+
+		Connection connection = null;
+
+		String url = prop.getProperty("url");
+		String username = prop.getProperty("username");
+		String password = prop.getProperty("password");
+
+		// Load DB driver
+		try {
+
+			Class.forName(driver).newInstance();
+
+		} catch (InstantiationException e) {
+
+			throw new NonRecoverableError(CANNOT_INSTANTIATE_DRIVER.getMessage());
+
+		} catch (IllegalAccessException e) {
+
+			throw new NonRecoverableError(CANNOT_INSTANTIATE_DRIVER.getMessage());
+
+		} catch (ClassNotFoundException e) {
+
+			throw new NonRecoverableError(CANNOT_FIND_DRIVER.getMessage());
+		}
+
+		// Create DB connection
+		try {
+
+			connection = DriverManager.getConnection(url, username, password);
+
+		} catch (SQLException e) {
+
+			throw new NonRecoverableError(CANNOT_CONNECT_DATABASE.getMessage());
+
+		}
+		return connection;
+	}
 	// Return the next valid objectID
 	public int getDocumentId() throws NonRecoverableError {
 
--- src/test/java/es/upm/grise/profundizacion2018/tema5/SmokeTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/tema5/SmokeTest.java
@@ -2,20 +2,54 @@ package es.upm.grise.profundizacion2018.tema5;
 
 import static org.junit.Assert.*;
 
+import java.util.Properties;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class SmokeTest {
-	
-	@Test
-	public void formatTemplateCorrectly() throws NonRecoverableError, RecoverableError {
-		
-		Document d = new Document();
-		d.setTemplate("DECLARATION");
-		d.setTitle("A");
-		d.setAuthor("B");
-		d.setBody("C");
-		assertEquals("DOCUMENT ID: 1115\n\nTitle : A\nAuthor: B\n\nC", d.getFormattedDocument());
 
+	//	@Test
+	//	public void formatTemplateCorrectly() throws NonRecoverableError, RecoverableError {
+	//		
+	//		Document d = new Document();
+	//		d.setTemplate("DECLARATION");
+	//		d.setTitle("A");
+	//		d.setAuthor("B");
+	//		d.setBody("C");
+	//		assertEquals("DOCUMENT ID: 1115\n\nTitle : A\nAuthor: B\n\nC", d.getFormattedDocument());
+	//
+	//	}
+	@Before public void initialize() {
+		//		 Properties prop = new Properties();
+		//			prop.setProperty("url", "");
+		//			prop.setProperty("username", "");
+		//			prop.setProperty("password", "");
+	}
+	
+	// PRUEBA UNITARIA Comprobación de que se genera bien el template
+	@Test
+	public void standardTemplateCreation() {
+		new TemplateFactory();
+		String template = TemplateFactory.getTemplate("DECLARATION");
+		String expected = "DOCUMENT ID: %d\n\nTitle : %s\nAuthor: %s\n\n%s";
+		assertEquals(template,expected );
+	}
+	// PRUEBA UNITARIA Con propiedades de configuración erroneas
+	@Test(expected = NonRecoverableError.class)
+	public void badProps() throws NonRecoverableError{
+		String driver = DocumentIdProvider.DRIVER;
+		DocumentIdProvider.getInstance(driver).readProp("badProps");
+	}
+	// PRUEBA UNITARIA Con driver errorneo
+	@Test(expected = NonRecoverableError.class)
+	public void badDriver() throws NonRecoverableError {
+		String driver = DocumentIdProvider.DRIVER;
+		Properties goodProps = new Properties();
+		goodProps.setProperty("url", "new");
+		goodProps.setProperty("username", "magasuan");
+		goodProps.setProperty("password", "123");
+		DocumentIdProvider.getInstance(driver).readFromDB("badDriver", goodProps);
 	}
 
 }
